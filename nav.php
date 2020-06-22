99a3269433530c3a2f87bd79e375f2d0a570556b
ROGER PC <rogelio.pc@gmail.com>
2020-06-22 04:23:46 -0500

    modificado localmente el archivo nav

---
+++ nav.php
@@ -0,0 +1,16 @@
+<nav>
+    <ol>
+    <li>
+        <a href="#">Active</a>
+    </li>
+    <li>
+        <a href="#">Link</a>
+    </li>
+    <li>
+        <a href="#">Link</a>
+    </li>
+    <li>
+        <a href="#">Disabled</a>
+    </li>
+    </ol>
+</nav>
