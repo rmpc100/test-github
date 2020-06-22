38372d1bcd42d91dcc7b5c59a91623c92da8abd9
rmpc100 <67265919+rmpc100@users.noreply.github.com>
2020-06-22 04:53:08 -0500

    Update nav.php
    Actualice el menú, agregue una nueva funcionalidad

--- nav.php
+++ nav.php
@@ -14,3 +14,9 @@
     </li>
     </ol>
 </nav>
+<nav class="nav">
+  <a class="nav-link active" href="#">Active</a>
+  <a class="nav-link" href="#">Link</a>
+  <a class="nav-link" href="#">Link</a>
+  <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+</nav>
